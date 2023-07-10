a420a6facf22cf3e1e57b1710f01c2e556f08b96
Vikas <vikas.3.singh-ext@contitech.ph>
2023-07-10 17:17:00 +0530

    Demo function app added

---
+++ DemoFunctionApp/Function1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net;
+
+namespace DemoFunctionApp
+{
+    public static class Function1
+    {
+        //[FunctionName("Function1")]
+        //public static async Task<IActionResult> Run(
+        //    [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        //    ILogger log)
+        //{
+        //    log.LogInformation("C# HTTP trigger function processed a request.");
+
+        //    string name = req.Query["name"];
+
+        //    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        //    dynamic data = JsonConvert.DeserializeObject(requestBody);
+        //    name = name ?? data?.name;
+
+        //    string responseMessage = string.IsNullOrEmpty(name)
+        //        ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+        //        : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+        //    return new OkObjectResult(responseMessage);
+        //}
+
+        [FunctionName("Function1")]
+        public static  HttpResponseMessage Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestMessage req)
+        {
+
+            return req.CreateResponse(HttpStatusCode.OK, "Heloo from Azure Function App!");
+        }
+    }
+}
